79cdeabaa1cbf8812077f066b3670b0b597066f4
nitsingh <nitisngh@paypal.com>
2013-06-22 15:44:21 -0700

    Code Cleanup

--- src/main/java/com/nicsin/gamedom/presentor/mvc/controller/HomeController.java
+++ src/main/java/com/nicsin/gamedom/presentor/mvc/controller/HomeController.java
@@ -15,19 +15,10 @@
  */
 package com.nicsin.gamedom.presentor.mvc.controller;
 
-import java.util.SortedSet;
-
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.nicsin.gamedom.presentor.model.TwitterMessage;
-import com.nicsin.gamedom.presentor.model.TwitterMessages;
-import com.nicsin.gamedom.presentor.service.TwitterService;
-import com.nicsin.gamedom.presentor.support.SortOrder;
 
 /**
  * Handles requests for the application home page.
@@ -42,39 +33,11 @@ public class HomeController {
 
 	private static final Logger LOGGER = Logger.getLogger(HomeController.class);
 
-	@Autowired
-	private TwitterService twitterService;
-
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value={"/", "/tweets"})
-	public String home(Model model, @RequestParam(required=false) Long latestTweetId,
-			@RequestParam(defaultValue="DESCENDING", required=false) SortOrder sortOrder) {
-
-		if (latestTweetId == null) {
-			latestTweetId = 0L;
-		}
-
-		final SortedSet<TwitterMessage> twitterMessages = twitterService.getTwitterMessages(latestTweetId, sortOrder);
-
-		TwitterMessages twitterMessagesWrapper = new TwitterMessages();
-
-		if (twitterMessages == null || twitterMessages.isEmpty()) {
-			twitterMessagesWrapper.setLatestTweetId(latestTweetId);
-		} else {
-			twitterMessagesWrapper.setTwitterMessages(twitterMessages);
-		}
-
-		twitterMessagesWrapper.setAdapterRunning(twitterService.isTwitterAdapterRunning());
-
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug(String.format("Latest Tweet ID: '%s'; Adapter running: %s",
-					twitterMessagesWrapper.getLatestTweetId(),
-					twitterMessagesWrapper.isAdapterRunning()));
-		}
-
-		model.addAttribute("tweets", twitterMessagesWrapper);
+	@RequestMapping(value={"/"})
+	public String home(Model model) {
 
 		return "home";
 	}
